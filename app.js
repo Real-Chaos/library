16ee4ad43accc3765fc1d31ccfffd21c411c7bb2
Chaos <rizwannafees13@gmail.com>
2021-03-23 10:28:40 -0400

    Completed navbar (still not responsive)

--- app.js
+++ app.js
@@ -1,82 +1,29 @@
-// Making Book Form Visible --------------------------------------------------------------------------------------------------------------------------------------
+// LightMode -------------------------------------------------------------------------------------------------------------------------------------------------------
 
-const bookForm = document.querySelector('.bookForm');
-const popupButton = document.querySelector('.bookBtn');
-const close = document.querySelector('.closeBtn');
+let lightMode = false;
+const themeChanger = document.querySelector('.lightMode');
 
-function makeBookFormVisible() {
-    popupButton.addEventListener('click', ()=> {
-        bookForm.style.display = 'flex';
-    });
-    
-    close.addEventListener('click', ()=> {
-        bookForm.style.display = 'none';
+function theme() {
+
+    themeChanger.addEventListener('click', ()=> {
+        if(lightMode === false) {
+            lightMode = true;
+            document.body.style.background = 'white';
+            themeChanger.classList.remove('fa-sun');
+            themeChanger.classList.add('fa-moon');
+            themeChanger.style.background = 'black';
+            themeChanger.style.color = 'white';
+        }
+        
+        else {
+            lightMode = false;
+            document.body.style.background = '#16151d';
+            themeChanger.classList.remove('fa-moon');
+            themeChanger.classList.add('fa-sun');
+            themeChanger.style.background = 'white';
+            themeChanger.style.color = 'black';
+        }
     })
 }
 
-makeBookFormVisible();
-
-// Storing Book --------------------------------------------------------------------------------------------------------------------------------------------------
-const submit = document.querySelector('.submitBtn')
-const bookTitle = document.querySelector('.bookTitle');
-const bookAuthor = document.querySelector('.bookAuthor');
-const bookPages = document.querySelector('.bookPages');
-const resetForm = document.querySelector('.form')
-
-let myLibrary = [];
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.info = ()=> {
-        return `${title}, ${author}, ${pages}`
-    }
-}
-
-function addBookToLibrary() {
-    submit.addEventListener('click', ()=>   {
-        let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
-        bookForm.style.display = 'none';
-        resetForm.reset()
-        myLibrary.push(newBook);
-    })
-}
-
-addBookToLibrary();
-
-// Creating Book Card -------------------------------------------------------------------------------------------------------------------------------------------
-
-const displayBooks = document.querySelector('.bookList');
-
-function creatingBook(title, author, pages) {
-    let bookCard = document.createElement('div');
-    let titlePlace = document.createElement('h1');
-    let authorPlace = document.createElement('h1');
-    let pageCount = document.createElement('h1');
-            
-    bookCard.classList.add('bookCard');
-    titlePlace.textContent = `" ${title} "`;
-    authorPlace.textContent = `By: " ${author} "`;
-    pageCount.textContent = `${pages} Pages`;
-            
-    displayBooks.appendChild(bookCard);
-    bookCard.appendChild(titlePlace);
-    bookCard.appendChild(authorPlace);
-    bookCard.appendChild(pageCount);
-}
-
-// Displaying Books ---------------------------------------------------------------------------------------------------------------------------------------------
-
-function displayBookList() {
-    submit.addEventListener('click', ()=> {
-        myLibrary.forEach(book => {
-            myLibrary = [];
-            creatingBook(book.title, book.author, book.pages);
-
-        })
-    })
-}
-
-displayBookList();
-
+theme();
